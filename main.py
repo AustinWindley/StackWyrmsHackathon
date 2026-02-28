8407801825ee5736655ce6797924fb8c248ec33b
JD Yergeau <Joseph.Yergeau@GMail.com>
2026-02-28 10:47:19 -0500

    basic flask server

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import Flask
+from flask import Flask
+import sqlite3
+from datetime import datetime
+
+app = Flask(__name__)
+
+database = sqlite3.connect('accounts.db')
+user_cursor = database.cursor()
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
