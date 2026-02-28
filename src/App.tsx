61165026f23f4f11db2b8ba3dc815c4e7cdc26bc
Austin <Austin@LAPTOP-4VS2LIO5>
2026-02-28 11:48:09 -0500

    Added pages to frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, createBrowserRouter, Routes, Route } from "react-router"
 import Home from "./Home.tsx"
+import Stocks from "./Stocks.tsx"
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/stocks" element={<Stocks />} /> 
+        {/* TODO: Implement Error Page */}
+        {/* <Route path="*" element={<ErrorPage />} /> */}
       </Routes>
     </BrowserRouter>
   )
---
+++ src/statics/Header.tsx
@@ -0,0 +1,8 @@
+import { AppBar, Box, Typography, Button, Avatar } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+import { useNavigate } from "react-router"
+
+export default function Header() {
+    const nav = useNavigate()
+
+}
