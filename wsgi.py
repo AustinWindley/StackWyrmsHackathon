24eb347eab0b8630abc500a31cf3694c787bd726
Austin <Austin@LAPTOP-4VS2LIO5>
2026-04-22 11:08:34 -0400

    set up for silk hosting

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+# import sys
+# from os import getcwd
+# cwd = getcwd()
+# sys.path.insert(0, cwd)
+from main import app as application
+# THIS FILE IS NECESSARY FOR THE SILK DEPLOYMENT SERVER TO ACCESS THE FLASK SERVER OBJECT
+# DO NOT ALTER OR TOUCH :D
